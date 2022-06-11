ad4cf3ac8b5d8d2189b19743d86f3ec0d309b455
Kareem Hassanein Hassan Abdelkreem <kareem.abdelkreem@ejust.edu.eg>
2022-06-11 22:17:52 +0200

    added simple SkipListNode

--- src/skiplist.hpp
+++ src/skiplist.hpp
@@ -1,5 +1,25 @@
 #ifndef   SKIPLIST_H
 #define   SKIPLIST_H
 
+#define MAX_LIST_SIZE 10
+#define MAX_LEVEL     10
+
+#include <random>
+
+struct SkipListNode
+{
+	SkipListNode()
+	{
+		this->data = 0;
+		this->next = new SkipListNode* [MAX_LIST_SIZE];
+		
+		for (int i = 0; i < MAX_LIST_SIZE; i ++)
+			this->next = nullptr;
+	}
+
+	int data;
+	SkipListNode **next;
+};
+
 #endif // SKIPLIST_H
 
