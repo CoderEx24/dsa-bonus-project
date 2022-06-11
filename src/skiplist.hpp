c4e04cf922104d2b05c85fb26c92d482bd555d6c
Kareem Hassanein Hassan Abdelkreem <kareem.abdelkreem@ejust.edu.eg>
2022-06-11 22:27:52 +0200

    added simple SkipList declaration

--- src/skiplist.hpp
+++ src/skiplist.hpp
@@ -11,7 +11,7 @@ struct SkipListNode
 	SkipListNode()
 	{
 		this->data = 0;
-		this->next = new SkipListNode* [MAX_LIST_SIZE];
+		this->next = new SkipListNode* [MAX_LEVEL];
 		
 		for (int i = 0; i < MAX_LIST_SIZE; i ++)
 			this->next = nullptr;
@@ -19,6 +19,26 @@ struct SkipListNode
 
 	int data;
 	SkipListNode **next;
+};
+
+class SkipList
+{
+	int h;
+	SkipListNode *sentinel;
+
+public:
+	SkipList(): h(0)
+	{
+		this->sentinel = new SkipListNode;
+	}
+	
+	SkipListNode* find_pred_node(int x);
+	unsigned int pick_height();
+
+public:
+	void insert(int x);
+	bool search(int x);
+	bool remove(int x);
 };
 
 #endif // SKIPLIST_H
