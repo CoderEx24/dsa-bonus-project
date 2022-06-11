5e9ab9d376a7605c0094aa8f54f925079f431700
Kareem Hassanein Hassan Abdelkreem <kareem.abdelkreem@ejust.edu.eg>
2022-06-11 20:58:06 +0200

    let's try again

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,17 @@
 
 int main()
 {
-	SkipList<int> skiplist;
-	skiplist.insert(1);
-	skiplist.insert(2);
-	skiplist.insert(3);
-	skiplist.search(0); // return false
-	skiplist.insert(4);
-	skiplist.search(1); // return true
-	skiplist.remove(0); // return false, 0 is not in skiplist
-	skiplist.remove(1); // return true
-	skiplist.search(1); // return false, 1 has already been deleted
+	SkipList<int> skiplist(100);
+	for (auto i : {21, 32, 43, 64, 75, 64, 37, 48, 59, 100})
+	{	
+		skiplist.insert(i);
+	}
+	skiplist.print();
+	//	skiplist.search(0); // return false
+//	skiplist.insert(4);
+//	skiplist.search(1); // return true
+//	skiplist.remove(0); // return false, 0 is not in skiplist
+//	skiplist.remove(1); // return true
+//	skiplist.search(1); // return false, 1 has already been deleted
 }
 
